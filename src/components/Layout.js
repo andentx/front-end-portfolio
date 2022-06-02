9cb0a2982236cdef894bfe975f276e6524af175d
Andrew <56039221+andentx@users.noreply.github.com>
2022-06-02 16:34:46 -0400

    Add CSS reset

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,45 @@ import * as React from 'react';
 
 import styled from 'styled-components';
 
+import { createGlobalStyle } from 'styled-components';
+
 import PageNavigation from '../components/PageNavigation';
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
+
+const GlobalReset = createGlobalStyle`
+ html,
+ body,
+ header,
+ main,
+ menu,
+ nav,
+ footer,
+ section,
+ article,
+ pre,
+ div,
+ h1,
+ h2,
+ h3,
+ h4,
+ h5,
+ h6,
+ p,
+ ol,
+ ul,
+ li,
+ a {
+     box-sizing: border-box;
+     margin: 0;
+     padding: 0;
+     border-style: none;
+     font-style: normal;
+     list-style: none;
+     text-decoration: none;
+     scroll-behavior: smooth;
+ }
+ `;
 
 const PageContainer = styled.div`
   background-color: darkred;
@@ -14,6 +50,7 @@ const PageContainer = styled.div`
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalReset />
       <PageContainer>
         <PageHeader />
         <PageNavigation />
