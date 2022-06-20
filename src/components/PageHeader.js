6354f41bbefb2c439228317360e98c0955171496
Andrew D <56039221+andent2@users.noreply.github.com>
2022-06-20 13:54:35 -0400

    Update page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,10 +1,27 @@
 import * as React from 'react';
 
+import styled from 'styled-components';
+
+const Header = styled.header`
+  background-color: yellow;
+  color: black;
+
+  height: 4rem;
+  width: 100%;
+  max-width: 1200px;
+
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const PageHeader = () => {
   return (
-    <div>
+    <Header>
       <h2>Page Header</h2>
-    </div>
+    </Header>
   );
 };
 
