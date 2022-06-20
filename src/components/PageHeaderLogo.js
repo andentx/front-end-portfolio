3e30ed74bc7328544966ee1f090ed80f0c498c6d
Andrew D <56039221+andent2@users.noreply.github.com>
2022-06-20 14:26:33 -0400

    Add letters to logo

--- src/components/PageHeaderLogo.js
+++ src/components/PageHeaderLogo.js
@@ -2,17 +2,41 @@ import * as React from 'react';
 
 import styled from 'styled-components';
 
-const Logo = styled.div`
+const LogoContainer = styled.div`
   background-color: green;
 
   height: 100%;
-  aspect-ratio: 1/1;
+
+  display: flex;
+`;
+
+const LetterA = styled.div`
+  height: 0;
+  width: 0;
+
+  border-left: 18.475px solid transparent;
+  border-right: 18.475px solid transparent;
+  border-bottom: 32px solid blue;
+
+  margin-right: 8px;
+`;
+
+const LetterD = styled.div`
+  height: 32px;
+  width: 0;
+
+  border-right: 32px solid blue;
+  border-top-right-radius: 32px;
+  border-bottom-right-radius: 32px;
 `;
 
 const PageHeaderLogo = () => {
   return (
     <>
-      <Logo></Logo>
+      <LogoContainer>
+        <LetterA />
+        <LetterD />
+      </LogoContainer>
     </>
   );
 };
